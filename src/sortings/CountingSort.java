06719f89047236455f55ea3a6771a857beb392cb
vgotsuliak <liddder@gmail.com>
2016-10-10 16:48:51 +0300

    made method for countingSort

--- src/sortings/CountingSort.java
+++ src/sortings/CountingSort.java
@@ -1,19 +1,29 @@
 package sortings;
 
+import java.util.Arrays;
+
 public class CountingSort {
 
     public static void main(String[] args) {
         int differentNumbersCount = 10;
         int[] randomArray = SortingUtils.getRandomArray(1000, differentNumbersCount);
+        int[] sortedArray = countingSort(randomArray, 10);
+        System.out.println(Arrays.toString(sortedArray));
+    }
+
+    public static int[] countingSort(int[] array, int differentNumbersCount) {
         int[] buckets = new int[differentNumbersCount+1];
-        for (int i = 0; i < randomArray.length; i++) {
-            buckets[randomArray[i]]++;
+        for (int i = 0; i < array.length; i++) {
+            buckets[array[i]]++;
         }
+        int index = 0;
         for (int i = 0; i < buckets.length; i++) {
             for (int j = 0; j < buckets[i]; j++) {
-                System.out.print(i);
+                array[index] = i;
+                index++;
             }
         }
+        return array;
     }
 
 }
