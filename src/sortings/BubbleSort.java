b6bd01acbf00645941bada670480002073195a3e
vgotsuliak <liddder@gmail.com>
2016-10-20 22:21:30 +0300

    fix bubble sort

--- src/sortings/BubbleSort.java
+++ src/sortings/BubbleSort.java
@@ -20,15 +20,13 @@ public class BubbleSort {
     public static int[] bubbleSort(int[] array) {
         int n = array.length - 1;
         for (int j = 0; j < n; j++) {
-            for (int i = 0; i < array.length - 1; i++) {
+            for (int i = 0; i < n; i++) {
                 if (array[i] > array[i + 1]) {
                     int temp = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = temp;
                 }
             }
-            printArrayInHtml(array, 50, "bubble.html", String.valueOf(j));
-            n--;
         }
         return array;
     }
